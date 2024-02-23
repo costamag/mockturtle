b7ac2ba98faacd98f5d08763288fb409a708fd7d
Andrea Costamagna <andrea.costamagna.ac@protonmail.com>
2024-02-23 17:33:25 +0100

    mig

--- experiments/basilsik_timing_opt.cpp
+++ experiments/basilsik_timing_opt.cpp
@@ -16,6 +16,7 @@
 #include <mockturtle/utils/stopwatch.hpp>
 #include <mockturtle/utils/tech_library.hpp>
 #include <mockturtle/algorithms/cleanup.hpp>
+#include <mockturtle/algorithms/mig_algebraic_rewriting.hpp>
 #include <mockturtle/algorithms/functional_reduction.hpp>
 #include <mockturtle/algorithms/klut_to_graph.hpp>
 #include <mockturtle/algorithms/balancing.hpp>
@@ -41,19 +42,27 @@ using namespace experiments;
 
 uint32_t CALL{0};
 //using experiment_t = experiments::experiment<std::string, uint32_t, uint32_t, std::string>;
-//experiment_t exp_res( "basilsik", "benchmark", "#gates", "depth", "method" );
+//experiment_t exp_res( "basilsik", "benchmark", "gates", "depth", "method" );
 
 bool read_file( sequential<aig_network>&, std::string const& );
-void print_commands();
+void print_aig_commands();
+void print_mig_commands();
+void print_end_commands();
 
 template<class Ntk>
-void print_stats( Ntk const& );
+void print_stats( Ntk const&, bool dot = false );
+
+bool optimize_aig( aig_network &, std::string const& );
+bool optimize_mig( depth_view<mig_network> &, std::string const& );
 
 template<class Ntk>
-bool optimize( Ntk &, std::string const& );
+void print_stats( binding_view<Ntk> const&, bool dot = false );
 
 template<class Ntk>
-void print_stats( binding_view<Ntk> const& );
+mig_network aig_to_mig( Ntk const& );
+
+template<class Ntk>
+aig_network mig_to_aig( Ntk const&, double required_time = std::numeric_limits<double>::max() );
 
 template<class Ntk>
 Ntk abc_opto( Ntk const&, std::string, std::string );
@@ -75,42 +84,60 @@ int main( int argc, char* argv[] )
   network_converters_stats st;
   aig_network aig = sequential_to_combinatorial( saig, st );
 
-  print_commands();
-
-  std::string command;
   while( true )
   {
-    getline( std::cin, command );
-    if( command == "map" )
+    print_aig_commands();
+
+    print_stats( aig, true );
+
+    std::string command;
+    while( true )
+    {
+      getline( std::cin, command );
+      if( command == "aig->mig" || command == "exit" )
+        break;
+      if( !optimize_aig( aig, command ) )
+        printf("wrong command\n");
+      
+      print_stats( aig, true );
+    }
+    if( command == "exit" )
       break;
-    if( !optimize( aig, command ) )
-      printf("wrong command\n");
-    
-    print_stats( aig );
+
+    print_mig_commands();
+
+    mig_network mig = aig_to_mig( aig );
+    print_stats( mig, true );
+    depth_view depth_mig{mig};
+
+    while( true )
+    {
+      getline( std::cin, command );
+      if( command == "mig->aig"  )
+        break;
+      if( !optimize_mig( depth_mig, command ) )
+        printf("wrong command\n");
+
+      print_stats( mig, true );
+    }
+
+    mig = cleanup_dangling(mig);
+    aig = mig_to_aig( mig, 0 );
+
+    print_end_commands();
+    getline( std::cin, command );
+    if( command == "exit" )
+      break;
   }
 
-  sequential<aig_network> saig2 = combinatorial_to_sequential( aig, st );
-  print_stats( saig2 );
+  printf("=================================\n");
+  printf("            AIG                  \n");
+  printf("---------------------------------\n");
 
-//  /* library to map to technology */
-//  std::vector<gate> gates;
-//  std::ifstream in( cell_libraries_path( "mcnc" ) );
-//
-//  if ( lorina::read_genlib( in, genlib_reader( gates ) ) != lorina::return_code::success )
-//  {
-//    return 1;
-//  }
-//
-//  tech_library_params tps;
-//  tech_library<5, classification_type::np_configurations> tech_lib( gates, tps );
-//  map_params ps2;
-//  ps2.cut_enumeration_ps.minimize_truth_table = true;
-//  ps2.cut_enumeration_ps.cut_limit = 24;
-//  map_stats st2;
-//
-//  binding_view<sequential<klut_network>> res2 = seq_map( dsaig, tech_lib, ps2, &st2 );
-//
-//  print_stats( res2 );
+  print_stats( aig, true );
+
+  sequential<aig_network> saig2 = combinatorial_to_sequential( aig, st );
+  //print_stats( saig2 );
 
   return 0;
 }
@@ -128,42 +155,177 @@ bool read_file( sequential<aig_network>& saig, std::string const& path )
   return 1;
 }
 
-void print_commands()
+void print_aig_commands()
 {
-  printf("===============================\n");
-  printf("map         : map to technology\n");
-  printf("abc-<script>: abc script\n");
-  printf("===============================\n");
+  printf("=================================\n");
+  printf("            AIG                  \n");
+  printf("---------------------------------\n");
+  printf("resyn2rs    |                    \n");
+  printf("it-resyn2rs | (resyn2rs)^infty   \n");
+  printf("compress2rs |                    \n");
+  printf("a-map       | if -a; fraig;      \n");
+  printf("d-map       | if -g; fraig;      \n");
+  printf("lazy        | if -y -K 6; fraig; \n");
+  printf("---------------------------------\n");
+  printf("exit                             \n");
+  printf("aig->mig    | map to MIGs        \n");
+  printf("---------------------------------\n\n");
+
+}
+
+void print_mig_commands()
+{
+  printf("=================================\n");
+  printf("            MIG                  \n");
+  printf("---------------------------------\n");
+  printf("dfs                              \n");
+  printf("selective                        \n");
+  printf("aggressive                       \n");
+  printf("---------------------------------\n");
+  printf("mig->aig    | map to MIGs        \n");
+  printf("---------------------------------\n");
+}
+
+void print_end_commands()
+{
+  printf("=================================\n");
+  printf("            END                  \n");
+  printf("---------------------------------\n");
+  printf("restart                          \n");
+  printf("exit                             \n");
+  printf("---------------------------------\n");
 }
 
 template<class Ntk>
-void print_stats( Ntk const& ntk )
+void print_stats( Ntk const& ntk, bool dot )
 {
   depth_view<Ntk> dntk{ntk};
-  printf("#gates = %5d #levels = %5d\n", dntk.num_gates(), dntk.depth() );
+  if( dot )
+    printf("> gates = %5d levels = %5d.\n", dntk.num_gates(), dntk.depth() );
+  else
+    printf("  gates = %5d levels = %5d\n", dntk.num_gates(), dntk.depth() );
+
 }
 
 template<class Ntk>
-void print_stats( binding_view<Ntk> const& bntk )
+void print_stats( binding_view<Ntk> const& bntk, bool dot )
 {
-  printf("area = %.2f delay = %.2f\n", bntk.compute_area(), bntk.compute_worst_delay() );
+  if( dot )
+    printf("> area = %.2f delay = %.2f.\n", bntk.compute_area(), bntk.compute_worst_delay() );
+  else
+    printf("  area = %.2f delay = %.2f\n", bntk.compute_area(), bntk.compute_worst_delay() );
 }
 
 #pragma endregion parsing
 
 #pragma region optimization
 
-template<class Ntk>
-bool optimize( Ntk& ntk, std::string const& cmd )
+bool optimize_mig( depth_view<mig_network>& depth_mig, std::string const& cmd )
 {
-  if( cmd.substr(0, 3) == "abc" )
+  if( cmd == "dfs" )
   {
-    std::string script = cmd.substr( 4, cmd.size()-1 );
-    auto tmp = abc_opto( ntk, cmd+std::to_string(CALL++), script );
+    mig_algebraic_depth_rewriting_params ps;
+    ps.strategy = mig_algebraic_depth_rewriting_params::dfs;
+    mig_algebraic_depth_rewriting( depth_mig, ps );
+    return true;
+  }
+  if( cmd == "aggressive" )
+  {
+    mig_algebraic_depth_rewriting_params ps;
+    ps.strategy = mig_algebraic_depth_rewriting_params::aggressive;
+    mig_algebraic_depth_rewriting( depth_mig, ps );
+    return true;
+  }
+  if( cmd == "selective" )
+  {
+    mig_algebraic_depth_rewriting_params ps;
+    ps.strategy = mig_algebraic_depth_rewriting_params::selective;
+    mig_algebraic_depth_rewriting( depth_mig, ps );
+    return true;
+  }
+  return false;
+}
+
+bool optimize_aig( aig_network& ntk, std::string const& cmd )
+{
+  if( cmd == "resyn2rs" )
+  {
+    auto tmp = abc_opto( ntk, cmd+std::to_string(CALL++), "resyn2rs" );
+    ntk = tmp;
+    return true;
+  }
+  if( cmd == "it-resyn2rs" )
+  {
+    int nT = ntk.num_gates()+1;
+    while( nT > ntk.num_gates() )
+    {
+      nT=ntk.num_gates();
+      ntk = abc_opto( ntk, cmd+std::to_string(CALL++), "resyn2rs" );
+      print_stats(ntk);
+    }
+    return true;
+  }  
+  if( cmd == "compress2rs" )
+  {
+    auto tmp = abc_opto( ntk, cmd+std::to_string(CALL++), "compress2rs" );
+    ntk = tmp;
+    return true;
+  }
+  if( cmd == "d-map" )
+  {
+    auto tmp = abc_opto( ntk, cmd+std::to_string(CALL++), "if -g; fraig;" );
+    ntk = tmp;
+    return true;
+  }
+  if( cmd == "a-map" )
+  {
+    auto tmp = abc_opto( ntk, cmd+std::to_string(CALL++), "if -a; fraig" );
+    ntk = tmp;
+    return true;
+  }
+  if( cmd == "lazy" )
+  {
+    auto tmp = abc_opto( ntk, cmd+std::to_string(CALL++), "if -y -K 6; fraig;" );
     ntk = tmp;
     return true;
   }
   return false;
+}
+
+template<class Ntk>
+mig_network aig_to_mig( Ntk const& aig )
+{
+  /* library to map to MIGs */
+  mig_npn_resynthesis resyn{ true };
+  exact_library_params eps;
+  eps.np_classification = true;
+  exact_library<mig_network> exact_lib( resyn, eps );
+
+  map_params ps1;
+  ps1.skip_delay_round = false;
+  ps1.required_time = std::numeric_limits<double>::max();
+  map_stats st1;
+
+  mig_network mig = map( aig, exact_lib, ps1, &st1 );
+  return mig;
+}
+
+template<class Ntk>
+aig_network mig_to_aig( Ntk const& mig, double required_time )
+{
+  /* library to map to MIGs */
+  xag_npn_resynthesis<aig_network, xag_network, xag_npn_db_kind::aig_complete> aig_resyn{};
+  exact_library_params eps;
+  eps.np_classification = true;
+  exact_library<aig_network> exact_lib(aig_resyn, eps);
+
+  map_params ps1;
+  ps1.skip_delay_round = false;
+  ps1.required_time = required_time;
+  map_stats st1;
+
+  aig_network aig = map( mig, exact_lib, ps1, &st1 );
+  return aig;
 }
 
 template<class Ntk>
