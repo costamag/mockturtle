1f1024b314bbbd6e905784bfeec6b13493cab361
Andrea Costamagna <costamag@github.com>
2025-06-02 17:07:28 +0200

    feat: mapped list

---
+++ include/mockturtle/utils/mapping/augmented_library.hpp
@@ -0,0 +1,101 @@
+/* mockturtle: C++ logic network library
+ * Copyright (C) 2018-2023  EPFL
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/*!
+  \file bind_library.hpp
+  \brief Implements methods for handling and evaluating a library of standerd cells.
+
+  This engine can be used for efficient Boolean evaluaton of the gates in a
+  standard cell library. Each gate is represented as an AIG index list for
+  efficient evaluation. Additionally, in the presence of multiple-output cells,
+  this engine identifies which gates belong to a multiple-output and allows handling
+  this information.
+
+  \author Andrea Costamagna
+*/
+
+#pragma once
+
+#include <vector>
+
+#include "../../algorithms/synth_engines/xag_synth.hpp"
+#include "../../utils/index_lists/lists/xag_index_list.hpp"
+#include "../../io/genlib_reader.hpp"
+
+namespace mockturtle
+{
+
+template<typename Gate>
+class augmented_library
+{
+public:
+  /* truth table used to express the gate functionality */
+  using func_t = kitty::dynamic_truth_table;
+  using list_t = large_xag_index_list;
+  /* gate type in the raw format */
+  using raw_gate_t = Gate;
+
+  /*! \brief Augmented gate.
+   *
+   * A raw gate is augmented by decomposing it into an XAIG index list for
+   * efficient simulation.
+   */
+  struct aug_gate_t : raw_gate_t
+  {
+    list_t aig_list;
+
+    aug_gate_t( const raw_gate_t& g, list_t const& list )
+      : raw_gate_t( g ), aig_list( list )
+  };
+  
+  augmented_library( std::vector<raw_gate_t> const& raw_gates )
+  {
+    xag_synth_stats st;
+    xag_synth_decompose synth( st );
+    aug_gates.reserve( raw_gates.size() );
+    for ( gate const& g : aug_gates )
+    {
+      synth( g.function );
+      list_t const list = synth.get_list();
+      aug_gates.emplace_back( g, list );
+    }
+  }
+
+  aug_gate_t const& get_augmented_gate( uint32_t id ) const
+  {
+    return aug_gates[id];
+  }
+
+  aug_gate_t const& get_list( uint32_t id ) const
+  {
+    return aug_gates[id].aig_list;
+  }
+
+private:
+/* Augmented technolofgy library */
+  std::vector<aug_gate_t> aug_gates;
+};
+
+} // namespace mockturtle
